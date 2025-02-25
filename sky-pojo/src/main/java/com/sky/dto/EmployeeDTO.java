fb456d0b4aa59ebad605c745558077edbaa2e6aa
Archer-stu <221404060102@stu.haust.edu.cn>
2025-02-25 21:50:58 +0800

    员工分页查询代码开发

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,10 +1,12 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel(description = "前端返回给后端的数据")
 public class EmployeeDTO implements Serializable {
 
     private Long id;
--- sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
@@ -1,10 +1,12 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel(description = "前端返回后端的相关数据")
 public class EmployeePageQueryDTO implements Serializable {
 
     //员工姓名
