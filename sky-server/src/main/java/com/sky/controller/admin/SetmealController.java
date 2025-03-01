5d086c042934cb7e168b8b2b71c4e8541492b473
Archer-stu <221404060102@stu.haust.edu.cn>
2025-03-01 23:09:44 +0800

    缓存套餐功能实现

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,6 +42,7 @@ public class SetmealController {
             return Result.success(pageResult);
 }
 @PostMapping
+@CacheEvict(cacheNames = "setmealCache",key = "#setmealDTO.categoryId")
     public Result save(@RequestBody SetmealDTO setmealDTO)
 {
     log.info("新增套餐：{}",setmealDTO);
@@ -48,12 +50,14 @@ public class SetmealController {
     return Result.success();
 }
 @PutMapping
+@CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO)
 {
     log.info("修改套餐：{}",setmealDTO);
     setmealService.update(setmealDTO);
     return Result.success();
 }
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
 @PostMapping("/status/{status}")
     public Result startOrStop(@PathVariable Integer status,Long id)
 {
@@ -62,8 +66,10 @@ public class SetmealController {
     return Result.success();
 }
 @DeleteMapping
+@CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result deleteByIds(@RequestParam List<Long> ids)
 {
+    log.info("批量删除套餐：{}",ids);
     setmealService.deleteByIds(ids);
     return Result.success();
 }
